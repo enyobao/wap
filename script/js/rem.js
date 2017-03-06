35d1ed30a33c14fd0e879639e2df717d093d929b
administator <administator>
2017-03-06 11:40:54 +0800

    rem.js

---
+++ script/js/rem.js
@@ -0,0 +1,14 @@
+/**
+ * Author:             Enyo Bao
+ * Version:            1.0
+ * Date                2017/3/6
+ * Discription:
+ */
+;(function(win,doc){
+    function change(){
+        doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/320+'px';
+        console.log("clientWidth:"+document.documentElement.clientWidth);
+    }
+    change();
+    win.addEventListener('resize',change,false);
+})(window,document);
